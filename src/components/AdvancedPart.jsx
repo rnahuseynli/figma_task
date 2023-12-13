036421d13905713e1dc53c315515c9ee6a23324f
rnahuseynli <bd7cr7q83@code.edu.az>
2023-12-13 13:21:42 +0400

    figma_task

---
+++ src/components/AdvancedPart.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const AdvancedPart = () => {
+  return (
+    <div className="middle_part">
+      <div className="middle_title">
+        <h1>Advanced Statistics</h1>
+        <p>
+          Track how your links are performing across the web with our advanced
+          statistics dashboard.
+        </p>
+      </div>
+      <div className="cards">
+        <div className="card_brand"></div>
+        <div className="card_detailed"></div>
+        <div className="card_fully"></div>
+      </div>
+    </div>
+  );
+};
+
+export default AdvancedPart;
